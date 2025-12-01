ca68ac5a212d6f1dcb3f23b8ce0833579e8e9fe7
younes-oss <aitchattouyounes@gmail.com>
2025-12-01 10:51:00 +0100

    second chance

---
+++ main.js
@@ -0,0 +1,64 @@
+const products = [
+  { id: 1, name: "Stylo", price: 5 },
+  { id: 2, name: "Cahier", price: 20 },
+  { id: 3, name: "Règle", price: 10 }
+];
+
+const productList = document.getElementById("productList");
+const addBtn = document.getElementById("addBtn");
+const searchInput = document.getElementById("searchInput");
+
+console.log(productList);
+
+function displayProducts(products){
+
+    productList.innerHTML =""
+    products.forEach(prod=>{
+        console.log(prod);
+        
+       const div = document.createElement("div");
+        div.innerHTML = `Stylo : ${prod.name} --- Prix : ${prod.price} DH `
+        productList.appendChild(div);
+
+    })
+    
+}
+
+function ajouterProduct(nameValue,prixValeur){
+    
+    const product = {
+        id: Date.now(),
+        name: nameValue,
+        price: prixValeur
+    };
+
+
+
+    products.push(product);
+
+}
+
+addBtn.addEventListener('click' ,()=>{
+
+    console.log("click")
+    const inputNom = document.getElementById("nameInput").value;
+    const inputPrix = document.getElementById("priceInput").value;
+
+    console.log(inputPrix);
+    
+
+    ajouterProduct(inputNom,inputPrix);
+    displayProducts(products);
+    
+})
+
+searchInput.addEventListener('input',()=>{
+    productList.innerHTML="";
+  const searchValue =  searchInput.value.toLocaleLowerCase();
+
+  let filtred = products.filter(prod=>prod.name.toLocaleLowerCase().includes(searchValue))
+
+    productList.innerHTML = displayProducts(filtred); 
+})
+
+displayProducts(products);
